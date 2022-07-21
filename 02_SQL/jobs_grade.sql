27a0d2d5503cf2f67766399ca8ceac5cc3b50dff
kyung yeol. in <inky4832@daum.net>
2022-07-21 11:35:28 +0900

    Add files via upload

---
+++ 02_SQL/jobs_grade.sql
@@ -0,0 +1,12 @@
+create table job_grades
+( grade_level varchar2(3) PRIMARY KEY,
+  lowest_sal  number,
+  highest_sal number );
+  
+insert into job_grades values ( 'A', 1000, 2999);
+insert into job_grades values ( 'B', 3000, 5999);
+insert into job_grades values ( 'C', 6000, 9999);
+insert into job_grades values ( 'D', 10000, 14999);
+insert into job_grades values ( 'E', 15000, 24999);
+insert into job_grades values ( 'F', 25000, 40000);
+commit;
